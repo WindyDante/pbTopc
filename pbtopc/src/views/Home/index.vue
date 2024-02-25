d30a114eca82e529dc0b0c5ce1c84fd07cfae26e
WindyDante <125888129+WindyDante@users.noreply.github.com>
2024-02-25 23:03:15 +0800

    add for code body,but is not a great

--- pbtopc/src/views/Home/index.vue
+++ pbtopc/src/views/Home/index.vue
@@ -43,8 +43,81 @@
           </div>
         </div>
       </el-header>
-      <el-main>Main</el-main>
-      <el-footer>Footer</el-footer>
+      <el-main>
+        <el-card
+          shadow="hover"
+          style="width: 1000px; height: 100px; margin: 0 auto"
+        >
+          <div>科技助力，农业发展更有活力</div>
+          <ul>
+            <li>
+              <ul style="display: flex">
+                <li style="flex: 12">“火焰蓝”开展应急演练 锤炼救援真本领</li>
+                <li style="flex: 12">
+                  检察公益诉讼多元破解老年人权益保障难题凝聚合力托起“夕阳红
+                </li>
+              </ul>
+            </li>
+            <li>
+              <ul style="display: flex">
+                <li style="flex: 12">
+                  媒体医声｜科普赋能，普陀区精神卫生中心做孩子的心灵“守门人
+                </li>
+                <li style="flex: 12">
+                  交通部门健全春运应急预案确保旅客安全出行
+                </li>
+              </ul>
+            </li>
+            <li>
+              <ul style="display: flex">
+                <li style="flex: 12">
+                  民生直通车丨功能更多元，社保卡将实现全国“一卡通”
+                </li>
+                <li style="flex: 12">深圳体彩去年筹集公益金21.10亿</li>
+              </ul>
+            </li>
+          </ul>
+        </el-card>
+        <el-carousel height="290px" style="width: 400px">
+          <el-carousel-item v-for="item in picList" :key="item.id">
+            <img :src="item.imgSrc" alt="" style="width: 100%" />
+          </el-carousel-item>
+        </el-carousel>
+        <!-- 详情页 -->
+        <div
+          style="
+            width: 1100px;
+            height: auto;
+            margin: 0 auto;
+            display: flex;
+            flex-wrap: wrap;
+          "
+        >
+          <ul
+            style="
+              border: 1px solid #ccc;
+              width: 350px;
+              height: 400px;
+              margin: 5px 0 0 5px;
+            "
+            v-for="item in picList2"
+            :key="item.id"
+          >
+            <li style="border-bottom: 1px solid #ccc">锐视</li>
+            <img
+              :src="item.imgSrc"
+              alt=""
+              style="width: 100%; height: 250px"
+            />
+            <li>边游边学</li>
+            <li>"应急救护 救在身边"</li>
+            <li>《华夏志愿者》 编辑部征稿启事</li>
+            <li>2022年度全国爱国卫生运动大会健康</li>
+            <li>98岁宝藏老人火了：在图书馆当</li>
+          </ul>
+        </div>
+      </el-main>
+      <el-footer> </el-footer>
     </el-container>
   </div>
 </template>
@@ -66,6 +139,17 @@ export default {
         "家具用品",
         "办公用品",
         "环保产品",
+      ],
+      picList: [
+        { id: 1, imgSrc: require("@/assets/images/pic1.png") },
+        { id: 2, imgSrc: require("@/assets/images/pic2.png") },
+        { id: 3, imgSrc: require("@/assets/images/pic3.jpg") },
+        { id: 4, imgSrc: require("@/assets/images/pic4.png") },
+        { id: 5, imgSrc: require("@/assets/images/pic5.png") },
+      ],
+      picList2: [
+        { id: 1, imgSrc: require("@/assets/images/pic6.jpg") },
+        { id: 2, imgSrc: require("@/assets/images/pic7.jpg") },
       ],
     };
   },
@@ -110,6 +194,9 @@ export default {
 </script>
 
 <style  scoped>
+li {
+  list-style-type: none;
+}
 .NavForOne {
   line-height: 60px;
   font-size: 16px;
